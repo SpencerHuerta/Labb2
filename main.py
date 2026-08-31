79a8a1ff010585a527515e221975e441587b0426
SpencerHuerta <spekh@kth.se>
2026-08-31 14:04:07 +0200

    Sista justering för kattis krav

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ from linkedQFile import LinkedQ # importera antingen linkedQFile eller arrayQFil
 # else:
 #     print("FAILED")
 
-kort = input('Mata in en kortföljd (separera med mellanslag): ').split() # Ber om en input
+kort = input().split() # Ber om en input
 q = LinkedQ() # Skapar ett länkat kö objekt alternativt ett array objekt
 for i in kort:
     q.enqueue(i) # Matar in varje element i kön 
@@ -34,5 +34,5 @@ for i in kort:
 while not q.isEmpty(): # körs till kön är tom
     q.enqueue(q.dequeue()) # Tar ut första elementet och lägger till det längst bak i kön
     x = q.dequeue() # tar bort första elementet i kön och skriver ut det.
-    print(x)
+    print(x, end= " ")
 
